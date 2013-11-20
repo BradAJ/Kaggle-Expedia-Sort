aabc7d2eeb0eb6ec7b07405f7389e28e9a60e2d9
Brad Jacobs <bradaj@gmail.com>
2013-11-19 23:19:26 -0800

    Collected funcs for kaggle_expedia_tools.py

--- kaggle_expedia_tools.py
+++ kaggle_expedia_tools.py
@@ -1,15 +1,16 @@
 import pandas as pd
+from pandas.io import sql
+import MySQLdb
 import numpy as np
 import sklearn.ensemble
-import sklearn.tree
 import math
 import pprint
-import MySQLdb
-from pandas.io import sql
-import cPickle as pkl
 
 
-def trainer(train_df, col_list, model = None, train_loc1 = 1, train_loc2 = 100020, cv_loc1 = 100021, cv_loc2 = 200009, print_factors = True):
+
+
+
+def trainer(train_df, col_list, model = None, train_loc1 = 7000014, train_loc2 = 9000007, cv_loc1 = 9000008, cv_loc2 = 9917530, print_factors = True):
     
     #exclude outlying prices for training.
     train_df = train_df.loc[(train_df['price_usd'] <= 2000.0) * (train_df['price_usd'] != 0.0)]
@@ -38,42 +39,6 @@ def trainer(train_df, col_list, model = None, train_loc1 = 1, train_loc2 = 10002
     else:
         return ndcg
 
-
-def df_from_query(col_list = None, row_limit = 200009, fill_na_0 = True, books_only = False):
-    """
-    Return a dataframe by calling the local Expedia MySQL database (Tables: PropFactors, UserSrchUnique, SrchUniversals) 
-    and returning a dataframe with the columns of interest in col_list (or a default set) along with ID and result columns: 
-    row_num, srch_id, booking_bool, click_bool.
-    The row_limit the rows are returned up to and including row_num = row_limit (where 2000009 cuts between searches 13431 and 13432).
-    Set row_limit = 9917530 to include the entire training sample.
-
-    Some of the combo columns may have div by 0 so convert NaN -> 0 by default.
-
-    big_query = "SELECT row_num, u.srch_id, booking_bool, click_bool, orig_destination_distance, price_usd, promotion_flag, prop_brand_bool, prop_country_id, prop_location_score1, prop_location_score2, prop_log_historical_price, prop_review_score, prop_starrating, random_bool, site_id, srch_adults_count, srch_booking_window, srch_children_count, srch_destination_id, srch_length_of_stay, srch_query_affinity_score, srch_room_count, srch_saturday_night_bool, visitor_hist_adr_usd, visitor_hist_starrating, visitor_location_country_id, prop_cnt, booking_cnt, click_cnt, promotion_cnt, train_price_avg, p.prop_id, (booking_cnt / prop_cnt) AS book_per_pcnt, (click_cnt / prop_cnt) AS click_per_pcnt, (promotion_flag / promotion_cnt) AS promo_per_procnt, ((booking_cnt - click_cnt) / prop_cnt) AS click_nobook_per_pcnt, (prop_review_score / price_usd) AS rev_by_price, (prop_starrating / price_usd) AS star_by_price, (train_price_avg / price_usd) AS avg_by_price FROM UserSrchUnique AS s, PropFactors7M AS p, SrchUniversals AS u WHERE p.prop_id = s.prop_id AND s.srch_id = u.srch_id AND row_num <=100019;"
-    """
-
-
-    if col_list == None:
-        def_q = ', '.join(default_cols)
-        agg_q = ', '.join(agg_cols)
-        query_cols = def_q +', '+ agg_q + ', ' + combo_cols_query
-    else:
-        query_cols = ', '.join(col_list) + ', ' + combo_cols_query
-    
-    if books_only == True:
-        query_str = 'SELECT row_num, u.srch_id, booking_bool, click_bool, ' + query_cols + ' FROM UserSrchUnique AS s, PropFactors7M AS p, SrchUniversals AS u WHERE p.prop_id = s.prop_id AND s.srch_id = u.srch_id AND s.booking_bool = 1 AND row_num <=' + str(row_limit) + ';'
-    else:
-        query_str = 'SELECT row_num, u.srch_id, booking_bool, click_bool, ' + query_cols + ' FROM UserSrchUnique AS s, PropFactors7M AS p, SrchUniversals AS u WHERE p.prop_id = s.prop_id AND s.srch_id = u.srch_id AND row_num <=' + str(row_limit) + ';'
-
-    print query_str
-    dbcon = MySQLdb.connect('localhost', 'kaggler', 'expediapass', 'expedia')
-
-    query_df = sql.frame_query(query_str, con = dbcon, index_col = 'row_num')
-    dbcon.close()
-
-    if fill_na_0:
-        query_df = query_df.fillna(value = 0)
-    return query_df
  
 def ndcg_calc(train_df, pred_scores):
     """
@@ -112,15 +77,65 @@ def ndcg_calc(train_df, pred_scores):
 
 
 
+def df_from_query(training = True):
+    
+    dbcon = MySQLdb.connect('localhost', 'kaggler', 'expediapass', 'expedia')
+
+    main_cols = "SELECT row_num, srch_id, s.prop_id, price_usd, prop_location_score1, prop_location_score2, prop_log_historical_price, prop_review_score, random_bool, srch_adults_count, srch_booking_window, srch_children_count, srch_length_of_stay, srch_query_affinity_score, srch_room_count, visitor_hist_adr_usd, (prop_starrating / price_usd) AS star_by_price, (prop_review_score / price_usd) AS rev_by_price"
+
+    agg_cols = ", prop_cnt, promotion_cnt, train_price_avg, (booking_cnt / prop_cnt) AS book_per_pcnt, (promotion_flag / promotion_cnt) AS promo_per_procnt, (train_price_avg / price_usd) AS avg_by_price, ((click_cnt - booking_cnt) / prop_cnt) AS click_nobook_per_pcnt"
+
+    agg_missing = ", 0 AS prop_cnt, 0 AS promotion_cnt, 0 AS train_price_avg, 0 AS book_per_pcnt, 0 AS promo_per_procnt, 0 AS avg_by_price, 0 AS click_nobook_per_pcnt"
+
+    if training:
+        from_tables1 = " FROM TrainSearch AS s, PropFactors7M AS p"
+        from_tables2 = " FROM TrainSearch AS s"
+    else:
+        from_tables1 = " FROM TestSearch AS s, PropFactors AS p"
+        from_tables2 = " FROM TestSearch AS s"
+
+    train_dfmost = sql.frame_query(main_cols + agg_cols + from_tables1 + " WHERE p.prop_id = s.prop_id AND srch_id >=" + str(srch_start) + " AND srch_id <= "+ str(srch_end)+";", con = dbcon, index_col = 'row_num')
+
+    train_dfpropmissing = sql.frame_query(main_cols + agg_missing + from_tables2 + " WHERE prop_id NOT IN (SELECT prop_id FROM PropFactors) AND srch_id >=" + str(srch_start) + " AND srch_id <= "+ str(srch_end)+";", con = dbcon, index_col = 'row_num')
+
+    dbcon.close()
+
+    train_df = pd.concat([train_dfmost, train_dfpropmissing])
+    train_df.sort(inplace = True)
+
+    train_df_agg = agg_by_srch(train_df)
+    train_df_agg.fillna(value = 0, inplace = True)
+
+    return train_df_agg
 
 
 
+def agg_by_srch(train_df):
+    cols_in = ['prop_location_score1', 
+               'prop_location_score2', 
+               'book_per_pcnt', 
+               'avg_by_price', 
+               'rev_by_price', 
+               'star_by_price', 
+               'promo_per_procnt',
+               'prop_log_historical_price', 
+               'click_nobook_per_pcnt', 
+               'ploc_score1_by_price', 
+               'ploc_score2_by_price']
 
+    cols_out = ['srch_ploc_score1_mean', 
+                'srch_ploc_score2_mean', 
+                'srch_book_per_pcnt_mean', 
+                'srch_avg_by_price_mean', 
+                'srch_rev_by_price_mean', 
+                'srch_star_by_price_mean', 
+                'srch_promo_per_procnt_mean', 
+                'srch_prop_log_historical_price_mean', 
+                'click_nobook_per_pcnt_mean', 
+                'ploc_score1_mean', 
+                'ploc_score2_mean']
 
-def agg_by_srch_v2(train_df, cols = None):
-    cols_in = ['prop_location_score1', 'prop_location_score2', 'book_per_pcnt', 'avg_by_price', 'rev_by_price', 'star_by_price', 'promo_per_procnt', 'prop_log_historical_price', 'click_nobook_per_pcnt']
     srch_df = train_df[['srch_id'] + cols_in].groupby(by='srch_id').mean()
-    cols_out = ['srch_ploc_score1_mean', 'srch_ploc_score2_mean', 'srch_book_per_pcnt_mean', 'srch_avg_by_price_mean', 'srch_rev_by_price_mean', 'srch_star_by_price_mean', 'srch_promo_per_procnt_mean', 'srch_prop_log_historical_price_mean', 'click_nobook_per_pcnt_mean']
     srch_df.columns = cols_out
 
     srch_df['srch_price_med'] = train_df[['srch_id','price_usd']].groupby(by = 'srch_id').median()
@@ -131,52 +146,59 @@ def agg_by_srch_v2(train_df, cols = None):
     cols_inp.append('price_usd')
     cols_outp.append('srch_price_med')
 
-    cols_agg = ['ploc_score1_by_mean', 'ploc_score2_by_mean', 'book_per_pcnt_by_mean', 'avg_by_price_by_mean', 'rev_by_price_by_mean', 'star_by_price_by_mean', 'promo_perprocnt_by_mean', 'loghistp_by_mean', 'click_nobper_pcnt_by_mean', 'price_by_med']
+    cols_agg = ['ploc_score1_by_mean', 
+                'ploc_score2_by_mean', 
+                'book_per_pcnt_by_mean', 
+                'avg_by_price_by_mean', 
+                'rev_by_price_by_mean', 
+                'star_by_price_by_mean', 
+                'promo_perprocnt_by_mean', 
+                'loghistp_by_mean', 
+                'click_nobper_pcnt_by_mean', 
+                'ploc_score1_by_price_by_mean', 
+                'ploc_score2_by_price_by_mean', 
+                'price_by_med']
 
     for i in xrange(len(cols_inp)):
         train_df[cols_agg[i]] = train_df[cols_inp[i]] / train_df.join(srch_df[['srch_id', cols_outp[i]]], on='srch_id', rsuffix = '_agg')[cols_outp[i]]
     
     train_df.fillna(value = 0, inplace = True)
-    
+    train_df.loc[train_df.price_by_med == np.inf, 'price_by_med'] = 0
 
-    if cols != None:
-        cols_ids = set(cols)
-        cols_ids.add('srch_id')
-        cols_ids.add('prop_id')
-        cols_ids.add('booking_bool')
-        cols_ids.add('click_bool')
-        return train_df[list(cols_ids)]
+    
+    return train_df
+
+
+def test_pred_sorted(test_df, model, cols, regress = False):
+    """
+    To save output use: test_out_df.to_csv("FILE OUT", index = False, cols = ['srch_id', 'prop_id'], header = ['SearchId','PropertyId'])
+    """
+    if regress:
+        scores = model.predict(test_df[cols])
     else:
-        return train_df
+        scores = model.predict_proba(test_df[cols])[:, 1]
+    test_df['sort_score'] = scores
+     
+    return test_df[['srch_id', 'prop_id', 'sort_score']].sort(columns=['srch_id', 'sort_score'], ascending = [True, False])
 
-def agg_by_srch_v3(train_df, cols = None):
-    cols_in = ['prop_location_score1', 'prop_location_score2', 'book_per_pcnt', 'avg_by_price', 'rev_by_price', 'star_by_price', 'promo_per_procnt', 'prop_log_historical_price', 'click_nobook_per_pcnt', 'ploc_score1_by_price', 'ploc_score2_by_price', 'srch_query_affinity_score']
-    srch_df = train_df[['srch_id'] + cols_in].groupby(by='srch_id').mean()
-    cols_out = ['srch_ploc_score1_mean', 'srch_ploc_score2_mean', 'srch_book_per_pcnt_mean', 'srch_avg_by_price_mean', 'srch_rev_by_price_mean', 'srch_star_by_price_mean', 'srch_promo_per_procnt_mean', 'srch_prop_log_historical_price_mean', 'click_nobook_per_pcnt_mean', 'ploc_score1_mean', 'ploc_score2_mean', 'affinity_score_mean']
-    srch_df.columns = cols_out
 
-    srch_df['srch_price_med'] = train_df[['srch_id','price_usd']].groupby(by = 'srch_id').median()
-    srch_df['srch_id'] = srch_df.index
+def loader_saver(model, cols, file_str = "test_preds_saved", regress_model = False):
+    """
+    Trying to run the MySQL query to load the entire Test set was too memory intensive for my 4Gb machine, 
+    so load and process the data in chunks.
+
+    Then join the files together on the command line:
+    cat test_preds_saved0.csv test_preds_saved1.csv test_preds_saved2.csv test_preds_saved3.csv test_preds_saved4.csv test_preds_saved5.csv > test_preds_all_ADDHEADER.csv
+    """
     
-    cols_inp = list(cols_in)
-    cols_outp = list(cols_out)
-    cols_inp.append('price_usd')
-    cols_outp.append('srch_price_med')
+    starts = [2, 110001, 220001, 330001, 440001, 550001]
+    ends = [110000, 220000, 330000, 440000, 550000, 700000]
 
-    cols_agg = ['ploc_score1_by_mean', 'ploc_score2_by_mean', 'book_per_pcnt_by_mean', 'avg_by_price_by_mean', 'rev_by_price_by_mean', 'star_by_price_by_mean', 'promo_perprocnt_by_mean', 'loghistp_by_mean', 'click_nobper_pcnt_by_mean', 'ploc_score1_by_price_by_mean', 'ploc_score2_by_price_by_mean', 'affinity_score_by_mean', 'price_by_med']
-
-    for i in xrange(len(cols_inp)):
-        train_df[cols_agg[i]] = train_df[cols_inp[i]] / train_df.join(srch_df[['srch_id', cols_outp[i]]], on='srch_id', rsuffix = '_agg')[cols_outp[i]]
-    
-    train_df.fillna(value = 0, inplace = True)
-    
-
-    if cols != None:
-        cols_ids = set(cols)
-        cols_ids.add('srch_id')
-        cols_ids.add('prop_id')
-        cols_ids.add('booking_bool')
-        cols_ids.add('click_bool')
-        return train_df[list(cols_ids)]
-    else:
-        return # train_df
+    for i in xrange(6):
+        test_df = test_df_out(cols, srch_start = starts[i], srch_end = ends[i])
+        
+        if regress_model:
+            pred_df = test_pred_sorted(test_df, model, cols, regress = True)
+        else:
+            pred_df = test_pred_sorted(test_df, model, cols, regress = False)
+        pred_df.to_csv(file_str + str(i) + ".csv", index = False, cols = ['srch_id', 'prop_id'], header = False)
